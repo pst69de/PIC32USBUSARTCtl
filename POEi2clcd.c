7c4e8656cf03d77843228e1b874d9f14fe6bfceb
Patrick O. Ehrmann <ehrmann@ipo-ps.de>
2015-03-20 16:43:14 +0100

    initial branching of I2C LCD interface separation

---
+++ POEi2clcd.c
@@ -0,0 +1,14 @@
+/* 
+ * File:    POEi2clcd.c
+ * Author:  Patrick O. Ehrmann (pst69@pst69.de)
+ * License: Creative Commons Zero (https://creativecommons.org/publicdomain/zero/1.0/)
+ *
+ * Created on 2015-03-20
+ * Description:
+ *   I2C LCD library
+ * Major Changes:
+ *   Version 0: alpha development
+ */
+
+
+
---
+++ POEi2clcd.h
@@ -0,0 +1,30 @@
+/* 
+ * File:    POEi2clcd.h
+ * Author:  Patrick O. Ehrmann (pst69@pst69.de)
+ * License: Creative Commons Zero (https://creativecommons.org/publicdomain/zero/1.0/)
+ *
+ * Created on 2015-03-20
+ * Description:
+ *   I2C LCD library
+ * Major Changes:
+ *   Version 0: alpha development
+ */
+
+#ifndef POEI2CLCD_H
+#define	POEI2CLCD_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include "plib_i2c.h"
+#include "system_config.h"
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* POEI2CLCD_H */
+
