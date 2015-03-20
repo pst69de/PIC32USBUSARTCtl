864b30a6b030d849d04c63ed81fd24fb17a8a284
Patrick O. Ehrmann <ehrmann@ipo-ps.de>
2015-03-20 17:12:49 +0100

    1st reorganizational moves

--- POEi2clcd.c
+++ POEi2clcd.c
@@ -10,5 +10,46 @@
  *   Version 0: alpha development
  */
 
+#include "POEi2clcd.h"
+
+// system_init calls
+
+void LCD_Initialize(void) {
+    PLIB_I2C_Disable(APP_LCD_I2C_ID);
+    // Prepare Ports I2C 1  
+    PLIB_PORTS_PinClear(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SCL_PIN);
+    PLIB_PORTS_PinClear(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SDA_PIN);
+    PLIB_PORTS_RemapOutput(APP_LCD_PORTS_ID, OUTPUT_FUNC_NO_CONNECT, OUTPUT_PIN_RPB8);
+    // open drain configuration I2C 1 Pins 5V tolerant, pull up with 2k2
+    PLIB_PORTS_PinOpenDrainEnable(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SCL_PIN);
+    PLIB_PORTS_PinOpenDrainEnable(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SDA_PIN);
+    //PLIB_PORTS_PinDirectionOutputSet(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SCL_PIN);
+    //PLIB_PORTS_PinDirectionOutputSet(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SDA_PIN);
+    // set the ports Input
+    PLIB_PORTS_PinDirectionInputSet(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SCL_PIN);
+    PLIB_PORTS_PinDirectionInputSet(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SDA_PIN);
+    // Configure General I2C Options
+    PLIB_I2C_SlaveClockStretchingEnable(APP_LCD_I2C_ID);
+    // Disable to be called 
+    PLIB_I2C_GeneralCallDisable(APP_LCD_I2C_ID);
+    // future option SMB compatibility
+    PLIB_I2C_SMBDisable(APP_LCD_I2C_ID);
+    // future option high baud rates (>100kHz)
+    PLIB_I2C_HighFrequencyEnable(APP_LCD_I2C_ID);
+    // only 7-Bit addresses
+    PLIB_I2C_ReservedAddressProtectDisable(APP_LCD_I2C_ID);
+    // Idle when proc idle (no operation in idle mode)
+    PLIB_I2C_StopInIdleDisable(APP_LCD_I2C_ID);
+    // Set Desired baud rate
+    PLIB_I2C_BaudRateSet( APP_LCD_I2C_ID, APP_SYSCLK_FREQ, APP_LCD_I2C_BAUD);
+    // Set the appropriate slave address (slave only)
+    //PLIB_I2C_SlaveAddress7BitSet(APP_LCD_I2C_ID, 0x7f);
+    // Optional:  Clear and enable interrupts before enabling the I2C module.
+    // Enable the module -> in SYS_Startup
+}
+
+// interrupt calls
+ 
+// app calls
 
 
--- system_config/sk_pic32_mx/system_init.c
+++ system_config/sk_pic32_mx/system_init.c
@@ -371,40 +371,6 @@ void LEDS_Initialize(void) {
     LEDW_OD;
 }
 
-void I2C_Initialize(void) {
-    PLIB_I2C_Disable(APP_LCD_I2C_ID);
-    // Prepare Ports I2C 1  
-    PLIB_PORTS_PinClear(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SCL_PIN);
-    PLIB_PORTS_PinClear(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SDA_PIN);
-    PLIB_PORTS_RemapOutput(APP_LCD_PORTS_ID, OUTPUT_FUNC_NO_CONNECT, OUTPUT_PIN_RPB8);
-    // open drain configuration I2C 1 Pins 5V tolerant, pull up with 2k2
-    PLIB_PORTS_PinOpenDrainEnable(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SCL_PIN);
-    PLIB_PORTS_PinOpenDrainEnable(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SDA_PIN);
-    //PLIB_PORTS_PinDirectionOutputSet(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SCL_PIN);
-    //PLIB_PORTS_PinDirectionOutputSet(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SDA_PIN);
-    // set the ports Input
-    PLIB_PORTS_PinDirectionInputSet(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SCL_PIN);
-    PLIB_PORTS_PinDirectionInputSet(APP_LCD_PORTS_ID, APP_LCD_PORT_CHANNEL, APP_LCD_SDA_PIN);
-    // Configure General I2C Options
-    PLIB_I2C_SlaveClockStretchingEnable(APP_LCD_I2C_ID);
-    // Disable to be called 
-    PLIB_I2C_GeneralCallDisable(APP_LCD_I2C_ID);
-    // future option SMB compatibility
-    PLIB_I2C_SMBDisable(APP_LCD_I2C_ID);
-    // future option high baud rates (>100kHz)
-    PLIB_I2C_HighFrequencyEnable(APP_LCD_I2C_ID);
-    // only 7-Bit addresses
-    PLIB_I2C_ReservedAddressProtectDisable(APP_LCD_I2C_ID);
-    // Idle when proc idle (no operation in idle mode)
-    PLIB_I2C_StopInIdleDisable(APP_LCD_I2C_ID);
-    // Set Desired baud rate
-    PLIB_I2C_BaudRateSet( APP_LCD_I2C_ID, APP_SYSCLK_FREQ, APP_LCD_I2C_BAUD);
-    // Set the appropriate slave address (slave only)
-    //PLIB_I2C_SlaveAddress7BitSet(APP_LCD_I2C_ID, 0x7f);
-    // Optional:  Clear and enable interrupts before enabling the I2C module.
-    // Enable the module -> in SYS_Startup
-}
-
 #ifdef APP_USE_UART
 void UART_Initialize(void) {
     // UART 2 RX
@@ -541,8 +507,8 @@ void SYS_Initialize ( void *data )
     // Check if the object returned by the device layer is valid
     SYS_ASSERT((SYS_MODULE_OBJ_INVALID != sysObjects.usbDevObject), "Invalid USB DEVICE object");
 #endif
-    // init I2C
-    I2C_Initialize();
+    // init LCD
+    LCD_Initialize();
 #ifdef APP_USE_UART
     // init UART
     UART_Initialize();
